b7ef08030e12082c69db627bf88e931699b3bf56
GustaMonteiro <gcvm6070@gmail.com>
2022-07-16 15:59:47 -0300

    Add cargo tools and gitconfig file

--- postInstall.sh
+++ postInstall.sh
@@ -31,6 +31,12 @@ LISTA_PROGRAMAS=(
   obs-studio
 )
 
+LISTA_PACOTES_CARGO=(
+  exa
+  git-delta
+  bat
+)
+
 instalar_yay () {
   cd /tmp
   git clone https://aur.archlinux.org/yay.git
@@ -45,12 +51,10 @@ instalar_pacotes () {
 }
 
 instalar_lunarvim () {
-  # yay -S neovim git yarn npm rust base-devel --noconfirm
   bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
 }
 
 instalar_zsh () {
-  # yay -S zsh zsh-theme-powerlevel10k-git ttf-meslo-nerd-font-powerlevel10k powerline-fonts awesome-terminal-fonts --noconfirm
   mkdir -p $ZSH_PLUGINS_PATH
   git clone https://github.com/z-shell/F-Sy-H $ZSH_PLUGINS_PATH/fast-syntax-highlighting
   git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_PLUGINS_PATH/zsh-autosuggestions
@@ -61,7 +65,10 @@ exports_e_sources () {
   echo "source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme" >> $ZSHRC
   echo "source $ZSH_PLUGINS_PATH/zsh-autosuggestions/zsh-autosuggestions.zsh" >> $ZSHRC
   echo "source $ZSH_PLUGINS_PATH/fast-syntax-highlighting/F-Sy-H.plugin.zsh" >> $ZSHRC
-  chsh -s $(which zsh)
+}
+
+instalar_pacotes_cargo () {
+  cargo install ${LISTA_PACOTES_CARGO[@]} 
 }
 
 instalar_yay
@@ -69,3 +76,6 @@ instalar_pacotes
 instalar_lunarvim
 instalar_zsh
 exports_e_sources
+instalar_pacotes_cargo
+
+chsh -s $(which zsh)
