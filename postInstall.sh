c25757a494a74329952dc0f8b9373bfc7d8b6265
GustaMonteiro <gcvm6070@gmail.com>
2022-07-16 14:54:37 -0300

    Add zshrc-alias, lvim-mappings and refactored .sh

--- postInstall.sh
+++ postInstall.sh
@@ -4,40 +4,56 @@ ZSH_PLUGINS_PATH="$HOME/.zsh-plugins"
 TEMPORARY_CLONE_PATH="/tmp"
 ZSHRC="$HOME/.zshrc"
 
-# atualizar o sistema
-sudo pacman -Syyuu --noconfirm
+LISTA_PROGRAMAS=(
+  google-chrome
+  spotify
+  sublime-text
+  texlive-most
+  texmaker
+  geogebra
+  flatpak
+  neovim
+  git
+  yarn
+  npm
+  rust
+  base-devel
+  zsh
+  zsh-theme-powerlevel10k-git
+  ttf-meslo-nerd-font-powerlevel10k
+  powerline-fonts
+  awesome-terminal-fonts
+)
 
-# lunarvim
-sudo pacman -S neovim git yarn npm rust base-devel --noconfirm
-bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
-echo 'export PATH=$HOME/.cargo/bin:$HOME/.local.bin:$PATH' >> $HOME/.bashrc # mover para o final
-echo 'export PATH=$HOME/.cargo/bin:$HOME/.local.bin:$PATH' >> $ZSHRC # mover para o final
+instalar_yay () {
+  cd /tmp
+  git clone https://aur.archlinux.org/yay.git
+  cd /tmp/yay
+  makepkg -si --noconfirm
+  yay -Syyuu --noconfirm
+  cd
+}
 
-# yay 
-cd /tmp
-git clone https://aur.archlinux.org/yay.git
-cd /tmp/yay
-makepkg -si --noconfirm
-yay -Syyuu --noconfirm
+instalar_pacotes () {
+  yay -S ${LISTA_PROGRAMAS[@]} --noconfirm
+}
 
-#zsh
-yay -S zsh zsh-theme-powerlevel10k-git ttf-meslo-nerd-font-powerlevel10k powerline-fonts awesome-terminal-fonts --noconfirm
-echo "source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme" >> $ZSHRC
-mkdir -p $ZSH_PLUGINS_PATH
-git clone https://github.com/z-shell/F-Sy-H $ZSH_PLUGINS_PATH/fast-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_PLUGINS_PATH/zsh-autosuggestions
-echo "source $ZSH_PLUGINS_PATH/zsh-autosuggestions/zsh-autosuggestions.zsh" >> $ZSHRC
-echo "source $ZSH_PLUGINS_PATH/fast-syntax-highlighting/F-Sy-H.plugin.zsh" >> $ZSHRC
+instalar_lunarvim () {
+  # yay -S neovim git yarn npm rust base-devel --noconfirm
+  bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
+}
 
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-# echo "zinit light zdharma/fast-syntax-highlighting" >> $HOME/.zshrc 
-# echo "zinit light zsh-users/zsh-autosuggestions" >> $HOME/.zshrc 
-# echo "zinit light zsh-users/zsh-completions" >> $HOME/.zshrc 
-chsh -s $(which zsh)
+instalar_zsh () {
+  # yay -S zsh zsh-theme-powerlevel10k-git ttf-meslo-nerd-font-powerlevel10k powerline-fonts awesome-terminal-fonts --noconfirm
+  mkdir -p $ZSH_PLUGINS_PATH
+  git clone https://github.com/z-shell/F-Sy-H $ZSH_PLUGINS_PATH/fast-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_PLUGINS_PATH/zsh-autosuggestions
+}
 
-# chrome
-yay -S google-chrome --noconfirm
-
-# flatpak
-yay -S flatpak --noconfirm
+exports_e_sources () {
+  echo 'export PATH=$HOME/.cargo/bin:$HOME/.local/bin:$PATH' >> $ZSHRC 
+  echo "source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme" >> $ZSHRC
+  echo "source $ZSH_PLUGINS_PATH/zsh-autosuggestions/zsh-autosuggestions.zsh" >> $ZSHRC
+  echo "source $ZSH_PLUGINS_PATH/fast-syntax-highlighting/F-Sy-H.plugin.zsh" >> $ZSHRC
+  chsh -s $(which zsh)
+}
